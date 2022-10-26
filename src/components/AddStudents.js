395f48ccf8aa22a03c1fba231403920ec00c66f6
Kaushik G Bhat <91201263+bhatkaushik@users.noreply.github.com>
2022-10-26 11:03:22 +0530

    Update AddStudents.js

--- src/components/AddStudents.js
+++ src/components/AddStudents.js
@@ -1,56 +1,70 @@
-import React,{useState} from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 function AddStudents(props) {
-    let navigate = useNavigate();
-    let [name,setName]=useState("");
-    let [email,setEmail]=useState("");
-    let [mobile,setMobile]=useState("");
-    let [cls,setCls]=useState("");
+  let navigate = useNavigate();
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [mobile, setMobile] = useState("");
+  let [cls, setCls] = useState("");
 
-    let handleSubmit = ()=>{
-        let newData = {name,email,mobile,"class":cls};
-        let newArray = [...props.data.students];
-        newArray.push(newData);
-        props.data.setStudents(newArray)
+  let handleSubmit = () => {
+    let newData = { name, email, mobile, class: cls };
+    let newArray = [...props.data.students];
+    newArray.push(newData);
+    props.data.setStudents(newArray);
 
-        navigate("/all-students")
+    navigate("/all-students");
+  };
 
-    }
+  return (
+    <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </Form.Group>
 
-    return (
-        <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Name" onChange={(e)=>setName(e.target.value)}/>
-            </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Mobile</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Mobile"
+          onChange={(e) => setMobile(e.target.value)}
+        />
+      </Form.Group>
 
-            <Form.Group className="mb-3">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)}/>
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Class</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Class"
+          onChange={(e) => setCls(e.target.value)}
+        />
+      </Form.Group>
 
-            <Form.Group className="mb-3" >
-                <Form.Label>Mobile</Form.Label>
-                <Form.Control type="text" placeholder="Mobile" onChange={(e)=>setMobile(e.target.value)}/>
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-                <Form.Label>Class</Form.Label>
-                <Form.Control type="text" placeholder="Class" onChange={(e)=>setCls(e.target.value)}/>
-            </Form.Group>
-  
-            <Button variant="primary" onClick={handleSubmit}>
-                Submit
-            </Button>
-        </Form>
-    )
+      <Button variant="primary" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
-export default AddStudents
+export default AddStudents;
